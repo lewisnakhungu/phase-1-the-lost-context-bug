6e1cc9f6fb9894f947c83d15a03190f9cf66ce82
Lewis <lokiyule@gmail.com>
2025-03-30 00:44:23 +0300

    solving the myth

--- index.js
+++ index.js
@@ -10,3 +10,70 @@ const messageConfig = {
       "Loki"
   ]
 };
+
+/*const printCard = function () {
+  console.log(this.frontContent);
+  console.log(this.insideContent);
+
+  this.signatories.forEach(function (signatory) {
+    const message = `${this.closing[signatory]}, ${signatory}`;
+    console.log(message);
+  });
+};
+
+printCard.call(messageConfig);*/
+
+/*const printCard = function () {
+  console.log(this.frontContent);
+  console.log(this.insideContent);
+
+  console.log("Debug Before forEach: " + this);
+  this.signatories.forEach(function (signatory) {
+    console.log("Debug Inside: " + this);
+    // const message = `${this.closing[signatory]}, ${signatory}`
+    // console.log(message)
+  });
+};
+
+printCard.call(messageConfig);*/
+
+/*const printCard = function () {
+  console.log(this.frontContent);
+  console.log(this.insideContent);
+
+  this.signatories.forEach(function (signatory) {
+    const message = `${this.closing[signatory]}, ${signatory}`;
+    console.log(message);
+  }, this);
+};
+
+printCard.call(messageConfig);*/
+
+/*
+Happy Birthday, Odin One-Eye!
+From Asgard to Nifelheim, you're the best all-father ever.
+
+Love,
+Admiration, respect, and love, Thor
+Your son, Loki
+*/
+const printCard = function () {
+  console.log(this.frontContent);
+  console.log(this.insideContent);
+  const contextBoundForEachExpr = function (signatory) {
+    const message = `${this.closing[signatory]}, ${signatory}`;
+    console.log(message);
+  }.bind(this);
+
+  this.signatories.forEach(contextBoundForEachExpr);
+};
+
+printCard.call(messageConfig);
+/*
+Happy Birthday, Odin One-Eye!
+From Asgard to Nifelheim, you're the best all-father ever.
+
+Love,
+Admiration, respect, and love, Thor
+Your son, Loki
+*/
